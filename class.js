c41f4085f2d949d216da2810689d3561f11e2605
unHealthy <chenzhutian1992@gmail.com>
2015-07-21 16:37:30 +0800

    dynamic class name

--- class.js
+++ class.js
@@ -9,12 +9,13 @@ class(stirng name,{
 //static
 //extend
 
-function BaseClass(className) {
-	this.className = className;
+//BaseClass(className)
+function BaseClass() {
+	this.numOfArgs = arguments.length;
 }
 
 BaseClass.prototype.GetClassName = function(){
-	return this.className;
+	return this.numOfArgs;
 }
 
 //Class(className,classContent)
@@ -23,13 +24,18 @@ function Class() {
 	var className = arguments[0];
 	var argsLength = arguments.length;
 	var classContents = arguments[argsLength - 1];
-	var superClass = BaseClass;
+	var superClass = argsLength === 3 ? arguments[1] : BaseClass;
 
-	var newClass =  new Function("superClass","className","return function " + className + "(){superClass.call(this,className);}")(superClass,className);
+	var newClass;
+	if(classContents.hasOwnProperty("constructor")){
+		newClass = new Function("superClass","constructor","return function " + className + "(){superClass.apply(this,arguments); constructor.apply(this,arguments);}")(superClass,classContents["constructor"]);
+		delete classContents["constructor"];
+	} else {
+		newClass = new Function("superClass","return function " + className + "(){superClass.apply(this,arguments);}")(superClass);
+	}
 	
 	//extends or not
 	if(argsLength === 3){
-		superClass = arguments[1];
 		for(var property in superClass){
 			if(superClass.hasOwnProperty(property)){
 				newClass[property] = superClass[property];
@@ -50,11 +56,14 @@ function Class() {
 }
 
 var A = Class("A",{
-			constructor:function(){},
+			constructor:function(){
+				this.ChildOfA = true;
+				console.log("param is "+arguments[0]);
+			},
 			member:1,
 			doSomething: function (){console.log(this.GetClassName()+" it is a function");}
 		});
-
+		
 var B = Class("B",A,{
 	childMember:2,
 	dododo:function(){console.log("Let's dodododo")}
@@ -62,15 +71,15 @@ var B = Class("B",A,{
 
 var C = Class("C",B,{});
 
-var a1 = new A();
+var a1 = new A("asdfsafdsadf");
 a1.doSomething();
 console.log(a1.GetClassName());
-var b = new B();
+var b = new B("bbbbbbbbb");
 b.doSomething();
 console.log(b.GetClassName());
-var a2 = new A();
+var a2 = new A("asdfsafd");
 console.log(a2.GetClassName());
-var c = new C();
+var c = new C("cccccccc");
 c.dododo();
 
 console.log(a1.GetClassName === b.GetClassName);
